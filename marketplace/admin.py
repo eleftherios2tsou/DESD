9a5acd6df45cb8dd475b50f67717aa23d002c21a
Gia Ngo <Gia2.Ngo@live.uwe.ac.uk>
2026-02-20 13:31:16 +0000

    bao

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-
+#bao
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
